4ce8cee35973182eb1f905b2e93b44b660c655f6
Teddy Wing <git@teddywing.com>
2023-12-15 01:55:21 +0100

    Increase version v1.1 -> v1.2

--- activity_bar.pl
+++ activity_bar.pl
@@ -22,7 +22,7 @@ use IO::Socket::INET;
 weechat::register(
 	'activity_bar',
 	'Teddy Wing',
-	'1.1',
+	'1.2',
 	'GPL-3.0-or-later',
 	'Monitor activity in all buffers and update AnyBar',
 	'shutdown',
